7b85f8b38450ade77813faac5e35f977329d47c8
Пользователь <lochinbekk77@gmail.com>
2024-05-03 00:28:01 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,166 @@
+// 1
+function a(a,b){
+    let s=a+b
+    console.log(s)
+}
+a(10,2)
+// 2
+function b (a){
+    let s=a*60
+    console.log(s)
+}
+b(5)
+// 3
+function d(a){
+    let s=a+1
+    console.log(s)
+}
+d(10)
+// 4
+function e(a,b){
+    let s=a*b/2
+    console.log(s)
+}
+e(10,10)
+// 5
+function f(a){
+    let s=a*365
+    console.log(s)
+}
+f(18)
+// 6
+function g(a){
+    let s=a**3
+    console.log(s)
+}
+g(10)
+// 7
+function h(a){
+    console.log(a[0])
+}
+h([1,2,3])
+// 8
+function i(a,b){
+    let s=a*b
+    console.log(s)
+
+}
+i(10,5)
+// 9
+function j(a,b){
+    let s=(a+b)-1
+    console.log(s)
+}
+j(10,6)
+// 10
+function k(a,b){
+    let s=(b/a)%1*10
+    console.log(Math.floor(s))
+}
+k(3,4)
+// 11
+function l(a,b){
+    let s=a*2
+    let z=b*2
+    console.log(s+z)
+
+}
+l(10,5)
+// 12
+function m(a){
+    console.log(`samsung ${a}`)
+}
+m('salom')
+// 13
+function n(a){
+    let s=a*a
+    console.log(s)
+}
+n(5)
+// 14
+function o(a){
+if(a<=0){
+    console.log('true')
+}else{
+    console.log('folse')
+}
+}
+o(-5)
+// 15
+function p(a){
+    let s=(a-2)*180
+    console.log(s)
+}
+p(6)
+// 16
+function q(a,b){
+    let s=a*2
+    let z=b*3
+    console.log(s+z)
+}
+q(7,5)
+// 17
+function r(a){
+console.log(a+' Edabit')
+}
+r('salom')
+// 18
+function s(a,b){
+let z=a+b
+if(z<=100){
+    console.log('true')
+}else{
+    console.log('folse')
+}
+}
+s(22,15)
+// 19
+function t(a){
+    let z=[]
+for(let i=1;i<=a;i++){
+z.push(i)
+}
+
+console.log(z)
+}
+t(10)
+// 20
+function u(a,b,s){
+    let z=a*2
+    let d=(b+s)*4
+    console.log(z+d)
+
+}
+u(2,3,4)
+// 21
+function v(a,b){
+    if(a&&b){
+        console.log('true')
+    }else{
+        console.log('folse')
+    }
+
+}
+v(true,false)
+// 22
+function x(a,b){
+    if(a===b){
+        console.log('true')
+    }else{
+        console.log('folse')
+    }
+}
+x(10,'10')
+// 23
+function y(a,b,s){
+let z=(a*3)+b
+console.log(z)
+}
+y(3,4,2)
+// 24
+function z(a,b){
+let z=a*60
+let x=b+z
+console.log(x*60)
+}
+z(1,3)
